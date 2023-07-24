5c32d19d95164119ba90ae40645c067c03dada1e
Akilan Gnanavel <akilan.gnanavel@incedoinc.com>
2023-07-24 15:33:23 -0500

    commit

---
+++ ProductService/src/main/java/com/productservice/productservice/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.productservice.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+	@GetMapping("/test")
+	public String test() {
+		return "Hello, world!";
+	}
+}
---
+++ SpringDiscovery/src/test/java/com/springdiscovery/springdiscovery/SpringDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdiscovery.springdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
